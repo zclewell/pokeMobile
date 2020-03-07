ce298a30f59de478ca71d7cd34a57bc8ad34ecca
zach <zclewell@gmail.com>
2020-03-07 14:34:26 -0600

    Implemented Generation selection

--- screens/DexScreen.js
+++ screens/DexScreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { FlatList, ScrollView, TouchableOpacity, TextInput } from 'react-native-gesture-handler';
 import DexListItem from '../components/DexListItem';
 import { _ } from 'lodash';
 
@@ -8,7 +8,7 @@ class GenSelectror extends React.Component {
     render() {
         return (
             <TouchableOpacity onPress={this.props.onPress}>
-                <View style={styles.genContainer}>
+                <View style={[styles.genContainer, this.props.selected === this.props.text ? styles.selected : styles.unselected]}>
                     <Text>{this.props.text}</Text>
                 </View>
             </TouchableOpacity>)
@@ -18,7 +18,7 @@ class GenSelectror extends React.Component {
 export default class DexScreen extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { navigation: props.navigation, range : _.range(1,894) }
+        this.state = { range : _.range(1,894), selected: "All"}
         console.log(props)
     }
 
@@ -36,21 +36,20 @@ export default class DexScreen extends React.Component {
                     showsHorizontalScrollIndicator={false}
                     style={{marginVertical: 8}}
                 >
-                    <GenSelectror text={"All"} onPress={() => this.setState({ range: _.range(1, 894) })} />
-                    <GenSelectror text={"Gen I"} onPress={() => this.setState({ range: _.range(1, 152) })}/>
-                    <GenSelectror text={"Gen II"} onPress={() => this.setState({ range: _.range(152, 252) })} />
-                    <GenSelectror text={"Gen III"} onPress={() => this.setState({ range: _.range(252, 387) })} />
-                    <GenSelectror text={"Gen IV"} onPress={() => this.setState({ range: _.range(387, 494) })}/>
-                    <GenSelectror text={"Gen V"} onPress={() => this.setState({ range: _.range(494, 650) })} />
-                    <GenSelectror text={"Gen VI"} onPress={() => this.setState({ range: _.range(650, 722) })} />
-                    <GenSelectror text={"Gen VII"} onPress={() => this.setState({ range: _.range(722, 810) })} />
-                    <GenSelectror text={"Gen VIII"} onPress={() => this.setState({ range: _.range(810, 891) })} />
-
+                    <GenSelectror text={"All"} onPress={() => this.setState({ range: _.range(1, 894), selected: 'All'})} selected={this.state.selected}/>
+                    <GenSelectror text={"Gen I"} onPress={() => this.setState({ range: _.range(1, 152), selected: "Gen I" })} selected={this.state.selected}/>
+                    <GenSelectror text={"Gen II"} onPress={() => this.setState({ range: _.range(152, 252), selected: "Gen II" })} selected={this.state.selected}/>
+                    <GenSelectror text={"Gen III"} onPress={() => this.setState({ range: _.range(252, 387), selected: "Gen III" })} selected={this.state.selected}/>
+                    <GenSelectror text={"Gen IV"} onPress={() => this.setState({ range: _.range(387, 494), selected: "Gen IV" })}selected={this.state.selected}/>
+                    <GenSelectror text={"Gen V"} onPress={() => this.setState({ range: _.range(494, 650), selected: "Gen V" })} selected={this.state.selected}/>
+                    <GenSelectror text={"Gen VI"} onPress={() => this.setState({ range: _.range(650, 722), selected: "Gen VI" })} selected={this.state.selected}/>
+                    <GenSelectror text={"Gen VII"} onPress={() => this.setState({ range: _.range(722, 810), selected: "Gen VII" })} selected={this.state.selected}/>
+                    <GenSelectror text={"Gen VIII"} onPress={() => this.setState({ range: _.range(810, 891), selected: "Gen VIII" })} selected={this.state.selected}/>
                 </ScrollView>
                 <FlatList
                     style={styles.flatList}
                     data={this.state.range}
-                    renderItem={({ item }) => <DexListItem key={item} number={item} navigation={this.state.navigation}/>}
+                    renderItem={({ item }) => <DexListItem key={item} number={item} navigation={this.props.navigation}/>}
                 />
             </View>
         );
@@ -66,8 +65,18 @@ const styles = StyleSheet.create({
     },
     genContainer: {
         padding: 10,
-        backgroundColor: '#f0f0f0',
         marginHorizontal: 4,
         borderRadius: 10
+    },
+    unselected: {
+        backgroundColor: '#e0e0e0'
+    },
+    selected: {
+        backgroundColor: '#fefefe'
+    },
+    searchContainer: {
+        
+        backgroundColor: 'red'
     }
+
 });
